89708b8a6b9afa8c637613e98f31ae01b7112c0c
AFRANIO JOSE SOUSA DE ALCANTARA <afranio.alcantara@viavarejo.com.br>
2023-06-13 21:27:32 -0300

    commit 01

---
+++ cypress/e2e/Ui/cadastro/cadastro.cy.js
@@ -0,0 +1,12 @@
+describe('testes na tela de cadastro',()=>{
+    beforeEach(()=>{
+        cy.visit('/login')
+    });
+    it('teste 01 cadastro com sucesso',()=>{
+        cy.get('[data-qa="signup-name"]').type('asdolfo')
+        cy.get('[data-qa="signup-email"]').type('aloprecioAStrogildo@123.com')
+        cy.get('[data-qa="signup-button"]').click()
+       
+    });
+
+});
